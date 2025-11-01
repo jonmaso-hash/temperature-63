55916dfc0a1aee990d315d09936664ccc17ef356
jonmaso-hash <jonmaso@gmail.com>
2025-11-01 14:58:08 -0700

    scripts

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+///let pi = 3.14;
+///let radius = 3;
+
+/// let area = pi * (radius *radius);
+
+///console.log(area);
+
+/// other solution for const
+const pi = 3.14;
+let radius = 3;
+
+let area = pi * (radius *radius);
+
+console.log(area);
