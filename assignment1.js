26863b640dbdb799942f04c3bd0877683af26dce
jonmaso-hash <jonmaso@gmail.com>
2025-11-04 18:13:18 -0800

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,17 @@
+let shoes = 2;
+let funnyGuy ="Clown";
+const age = 45;
+const height = 175;
+let place = "Royal Park";
+
+console.log(" The "+ funnyGuy + " pretended to be a giant by standing on a ladder "+"at "+ place + ", even though he was only " +height + "cm tall."+ " It was hilarous "+ "he made the face of a crying baby and appeared younger then his actual age of " + age);
+
+let man1 = "priest";
+let man2 = "rabbi";
+let man3 = "pastor";
+let building = "bar";
+
+
+
+
+console.log("A " + man1 + ", a "+ man2 +", and a "+ man3 +" are sitting at a " + building + ". The "+ man1 +" asks the "+ man2  + " to borrow $5 to pay for his Château Margau. The " + man2  +" asks the " + man3 +" to borrow $10 to pay for his Marchesi Antinori...the "+ man3 +" gets on his knees and start praying, the "+  man1 +" and the "+ man2 +" follow...They all stand and open thier eyes. The bartender then asks...why are you guys so worried, water is on the house?");
